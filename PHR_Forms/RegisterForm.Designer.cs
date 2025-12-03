7aaa6acdcb26bdd6f2725cc81b935be0dc4f8fc7
varbc <lbc3420@gmail.com>
2025-12-03 23:28:23 +0900

    Revert "Merge branch 'develop' into Input/Hospital"
    This reverts commit 6f9cee6bf80fda4238350ab132c1c12955ffba23, reversing
    changes made to 4db2e4580a08a8e9ec5fa1a838e47798827e6d4f.

--- PHR_Forms/RegisterForm.Designer.cs
+++ PHR_Forms/RegisterForm.Designer.cs
@@ -46,109 +46,97 @@
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(23, 19);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(33, 29);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(139, 21);
+            this.label1.Size = new System.Drawing.Size(199, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "회원가입 페이지";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(210, 62);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(300, 93);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(16, 12);
+            this.label2.Size = new System.Drawing.Size(22, 18);
             this.label2.TabIndex = 1;
             this.label2.Text = "ID";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(162, 94);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(232, 141);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 12);
+            this.label3.Size = new System.Drawing.Size(90, 18);
             this.label3.TabIndex = 1;
             this.label3.Text = "Password";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(195, 132);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(278, 198);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.Size = new System.Drawing.Size(44, 18);
             this.label4.TabIndex = 1;
             this.label4.Text = "이름";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(184, 205);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(263, 308);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(43, 12);
+            this.label6.Size = new System.Drawing.Size(59, 18);
             this.label6.TabIndex = 1;
             this.label6.Text = "E-Mail";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(169, 167);
-            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Location = new System.Drawing.Point(242, 251);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.Size = new System.Drawing.Size(80, 18);
             this.label8.TabIndex = 1;
             this.label8.Text = "전화번호";
             // 
             // txtId
             // 
-            this.txtId.Location = new System.Drawing.Point(255, 60);
-            this.txtId.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtId.Location = new System.Drawing.Point(364, 90);
             this.txtId.Name = "txtId";
-            this.txtId.Size = new System.Drawing.Size(128, 21);
+            this.txtId.Size = new System.Drawing.Size(181, 28);
             this.txtId.TabIndex = 2;
             // 
             // txtPw
             // 
-            this.txtPw.Location = new System.Drawing.Point(255, 92);
-            this.txtPw.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPw.Location = new System.Drawing.Point(364, 138);
             this.txtPw.Name = "txtPw";
-            this.txtPw.Size = new System.Drawing.Size(128, 21);
+            this.txtPw.Size = new System.Drawing.Size(181, 28);
             this.txtPw.TabIndex = 2;
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(255, 130);
-            this.txtName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtName.Location = new System.Drawing.Point(364, 195);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(128, 21);
+            this.txtName.Size = new System.Drawing.Size(181, 28);
             this.txtName.TabIndex = 2;
             // 
             // txtPhone
             // 
-            this.txtPhone.Location = new System.Drawing.Point(255, 165);
-            this.txtPhone.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPhone.Location = new System.Drawing.Point(364, 248);
             this.txtPhone.Name = "txtPhone";
-            this.txtPhone.Size = new System.Drawing.Size(128, 21);
+            this.txtPhone.Size = new System.Drawing.Size(181, 28);
             this.txtPhone.TabIndex = 2;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(255, 203);
-            this.txtEmail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtEmail.Location = new System.Drawing.Point(364, 305);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(128, 21);
+            this.txtEmail.Size = new System.Drawing.Size(181, 28);
             this.txtEmail.TabIndex = 2;
             // 
             // btnRegist
             // 
-            this.btnRegist.Location = new System.Drawing.Point(164, 249);
-            this.btnRegist.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnRegist.Location = new System.Drawing.Point(235, 373);
             this.btnRegist.Name = "btnRegist";
-            this.btnRegist.Size = new System.Drawing.Size(65, 29);
+            this.btnRegist.Size = new System.Drawing.Size(93, 43);
             this.btnRegist.TabIndex = 3;
             this.btnRegist.Text = "회원가입";
             this.btnRegist.UseVisualStyleBackColor = true;
@@ -156,10 +144,9 @@
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(295, 249);
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCancel.Location = new System.Drawing.Point(422, 373);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(65, 29);
+            this.btnCancel.Size = new System.Drawing.Size(93, 43);
             this.btnCancel.TabIndex = 3;
             this.btnCancel.Text = "취소";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -167,9 +154,9 @@
             // 
             // RegisterForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(560, 300);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnRegist);
             this.Controls.Add(this.txtEmail);
@@ -183,7 +170,6 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "RegisterForm";
             this.Text = "RegisterForm";
             this.ResumeLayout(false);
