4db2e4580a08a8e9ec5fa1a838e47798827e6d4f
varbc <lbc3420@gmail.com>
2025-12-03 22:20:42 +0900

    하아 기능은 동작합니다. 머지해보고 안되는거 말씀해주세요

--- PHR_Forms/HealthInputForm.Designer.cs
+++ PHR_Forms/HealthInputForm.Designer.cs
@@ -58,7 +58,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label9.Location = new System.Drawing.Point(589, 22);
+            this.label9.Location = new System.Drawing.Point(309, 14);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(191, 24);
             this.label9.TabIndex = 10;
@@ -68,7 +68,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label10.Location = new System.Drawing.Point(522, 216);
+            this.label10.Location = new System.Drawing.Point(242, 208);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(46, 13);
             this.label10.TabIndex = 15;
@@ -78,7 +78,7 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label11.Location = new System.Drawing.Point(522, 181);
+            this.label11.Location = new System.Drawing.Point(242, 173);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(20, 13);
             this.label11.TabIndex = 14;
@@ -88,7 +88,7 @@
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label12.Location = new System.Drawing.Point(522, 143);
+            this.label12.Location = new System.Drawing.Point(242, 135);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(33, 13);
             this.label12.TabIndex = 13;
@@ -98,7 +98,7 @@
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label13.Location = new System.Drawing.Point(522, 103);
+            this.label13.Location = new System.Drawing.Point(242, 95);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(33, 13);
             this.label13.TabIndex = 12;
@@ -108,7 +108,7 @@
             // 
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label14.Location = new System.Drawing.Point(522, 67);
+            this.label14.Location = new System.Drawing.Point(242, 59);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(33, 13);
             this.label14.TabIndex = 11;
@@ -116,7 +116,7 @@
             // 
             // DataAddbtn
             // 
-            this.DataAddbtn.Location = new System.Drawing.Point(633, 249);
+            this.DataAddbtn.Location = new System.Drawing.Point(353, 241);
             this.DataAddbtn.Name = "DataAddbtn";
             this.DataAddbtn.Size = new System.Drawing.Size(90, 27);
             this.DataAddbtn.TabIndex = 16;
@@ -128,7 +128,7 @@
             // 
             this.label15.AutoSize = true;
             this.label15.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label15.Location = new System.Drawing.Point(746, 332);
+            this.label15.Location = new System.Drawing.Point(458, 324);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(46, 13);
             this.label15.TabIndex = 20;
@@ -138,7 +138,7 @@
             // 
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label16.Location = new System.Drawing.Point(524, 368);
+            this.label16.Location = new System.Drawing.Point(244, 360);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(76, 13);
             this.label16.TabIndex = 19;
@@ -148,7 +148,7 @@
             // 
             this.label17.AutoSize = true;
             this.label17.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label17.Location = new System.Drawing.Point(524, 332);
+            this.label17.Location = new System.Drawing.Point(244, 324);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(46, 13);
             this.label17.TabIndex = 18;
@@ -158,7 +158,7 @@
             // 
             this.label18.AutoSize = true;
             this.label18.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label18.Location = new System.Drawing.Point(524, 292);
+            this.label18.Location = new System.Drawing.Point(244, 284);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(76, 13);
             this.label18.TabIndex = 17;
@@ -167,14 +167,14 @@
             // cmbThresholdItem
             // 
             this.cmbThresholdItem.FormattingEnabled = true;
-            this.cmbThresholdItem.Location = new System.Drawing.Point(603, 290);
+            this.cmbThresholdItem.Location = new System.Drawing.Point(323, 282);
             this.cmbThresholdItem.Name = "cmbThresholdItem";
             this.cmbThresholdItem.Size = new System.Drawing.Size(120, 20);
             this.cmbThresholdItem.TabIndex = 21;
             // 
             // Thresholdbtn
             // 
-            this.Thresholdbtn.Location = new System.Drawing.Point(749, 292);
+            this.Thresholdbtn.Location = new System.Drawing.Point(461, 284);
             this.Thresholdbtn.Name = "Thresholdbtn";
             this.Thresholdbtn.Size = new System.Drawing.Size(110, 20);
             this.Thresholdbtn.TabIndex = 22;
@@ -184,42 +184,42 @@
             // 
             // txtSystolic
             // 
-            this.txtSystolic.Location = new System.Drawing.Point(593, 65);
+            this.txtSystolic.Location = new System.Drawing.Point(313, 57);
             this.txtSystolic.Name = "txtSystolic";
             this.txtSystolic.Size = new System.Drawing.Size(175, 21);
             this.txtSystolic.TabIndex = 34;
             // 
             // txtBloodSugar
             // 
-            this.txtBloodSugar.Location = new System.Drawing.Point(593, 101);
+            this.txtBloodSugar.Location = new System.Drawing.Point(313, 93);
             this.txtBloodSugar.Name = "txtBloodSugar";
             this.txtBloodSugar.Size = new System.Drawing.Size(175, 21);
             this.txtBloodSugar.TabIndex = 35;
             // 
             // txtWeight
             // 
-            this.txtWeight.Location = new System.Drawing.Point(593, 141);
+            this.txtWeight.Location = new System.Drawing.Point(313, 133);
             this.txtWeight.Name = "txtWeight";
             this.txtWeight.Size = new System.Drawing.Size(175, 21);
             this.txtWeight.TabIndex = 36;
             // 
             // txtHeight
             // 
-            this.txtHeight.Location = new System.Drawing.Point(593, 179);
+            this.txtHeight.Location = new System.Drawing.Point(313, 171);
             this.txtHeight.Name = "txtHeight";
             this.txtHeight.Size = new System.Drawing.Size(175, 21);
             this.txtHeight.TabIndex = 37;
             // 
             // txtHeartRate
             // 
-            this.txtHeartRate.Location = new System.Drawing.Point(593, 214);
+            this.txtHeartRate.Location = new System.Drawing.Point(313, 206);
             this.txtHeartRate.Name = "txtHeartRate";
             this.txtHeartRate.Size = new System.Drawing.Size(175, 21);
             this.txtHeartRate.TabIndex = 38;
             // 
             // DataLoadbtn
             // 
-            this.DataLoadbtn.Location = new System.Drawing.Point(408, 474);
+            this.DataLoadbtn.Location = new System.Drawing.Point(128, 466);
             this.DataLoadbtn.Name = "DataLoadbtn";
             this.DataLoadbtn.Size = new System.Drawing.Size(90, 27);
             this.DataLoadbtn.TabIndex = 39;
@@ -228,21 +228,23 @@
             // 
             // txtUpperBound
             // 
-            this.txtUpperBound.Location = new System.Drawing.Point(603, 325);
+            this.txtUpperBound.Location = new System.Drawing.Point(323, 317);
             this.txtUpperBound.Name = "txtUpperBound";
+            this.txtUpperBound.ReadOnly = true;
             this.txtUpperBound.Size = new System.Drawing.Size(120, 21);
             this.txtUpperBound.TabIndex = 40;
             // 
             // txtLowerBound
             // 
-            this.txtLowerBound.Location = new System.Drawing.Point(798, 330);
+            this.txtLowerBound.Location = new System.Drawing.Point(510, 317);
             this.txtLowerBound.Name = "txtLowerBound";
+            this.txtLowerBound.ReadOnly = true;
             this.txtLowerBound.Size = new System.Drawing.Size(120, 21);
             this.txtLowerBound.TabIndex = 41;
             // 
             // HospitalAddbtn
             // 
-            this.HospitalAddbtn.Location = new System.Drawing.Point(603, 362);
+            this.HospitalAddbtn.Location = new System.Drawing.Point(323, 354);
             this.HospitalAddbtn.Name = "HospitalAddbtn";
             this.HospitalAddbtn.Size = new System.Drawing.Size(117, 27);
             this.HospitalAddbtn.TabIndex = 42;
@@ -253,7 +255,7 @@
             // dataGridHospitalLog
             // 
             this.dataGridHospitalLog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridHospitalLog.Location = new System.Drawing.Point(528, 395);
+            this.dataGridHospitalLog.Location = new System.Drawing.Point(248, 387);
             this.dataGridHospitalLog.Name = "dataGridHospitalLog";
             this.dataGridHospitalLog.RowTemplate.Height = 23;
             this.dataGridHospitalLog.Size = new System.Drawing.Size(397, 150);
@@ -263,7 +265,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(952, 564);
+            this.ClientSize = new System.Drawing.Size(837, 564);
             this.Controls.Add(this.dataGridHospitalLog);
             this.Controls.Add(this.HospitalAddbtn);
             this.Controls.Add(this.txtLowerBound);
--- PHR_Forms/HealthInputForm.cs
+++ PHR_Forms/HealthInputForm.cs
@@ -15,6 +15,13 @@ namespace PHR_Management_App
         // 실제 구현에서는 로그인 사용자 ID를 사용해야 합니다.
         private string mockMemberId = "M001"; // 사용자 ID (로그인 세션에서 가져와야 함)
         private Dictionary<TextBox, string> healthItemMap;
+        Dictionary<string, string> itemDict = new Dictionary<string, string>()
+        {
+            { "혈당 수치", "IT001" },
+            { "수축기 혈압", "IT002" },
+            { "체온", "IT003" }
+            // 필요한 경우 더 많은 항목 추가
+        };
         // ====================================================================
         // 생성자 및 초기화
         // ====================================================================
@@ -22,7 +29,7 @@ namespace PHR_Management_App
         {
             // Designer.cs에 정의된 UI 요소를 초기화합니다.
             InitializeComponent();
-
+            InitializeDataInputs();
             InitializeHealthItemMap();
             dbc = dbClass;
             this.Load += HealthInputForm_Load;
@@ -64,36 +71,129 @@ namespace PHR_Management_App
                 DataAddbtn.Click += DataAddbtn_Click;
             }
         }
+        private void InitializeDataInputs()
+        {
+                      // --- 2. 임계치 항목 콤보박스 초기화 (cmbThresholdItem) ---
+            if (cmbThresholdItem != null)
+            {
+                cmbThresholdItem.Items.Clear();
+                foreach (string itemName in itemDict.Keys)
+                {
+                    cmbThresholdItem.Items.Add(itemName);
+                }
+                if (cmbThresholdItem.Items.Count > 0)
+                {
+                    cmbThresholdItem.SelectedIndex = 0;
+                }
+            }
+
+        }
+        private void Thresholdbtn_Click(object sender, EventArgs e)
+        {
+            // 1. 선택된 항목의 ITEM_CODE 가져오기
+            string selectedItemName = cmbThresholdItem.SelectedItem?.ToString();
+            if (string.IsNullOrEmpty(selectedItemName) || !itemDict.ContainsKey(selectedItemName))
+            {
+                MessageBox.Show("임계치 유형을 선택해주세요.", "선택 오류");
+                return;
+            }
+            string itemCode = itemDict[selectedItemName];
+
+            // 초기화
+            txtUpperBound.Clear();
+            txtLowerBound.Clear();
+
+            string sql = @"
+                SELECT 
+                    UPPER_BOUND, 
+                    LOWER_BOUND 
+                FROM THRESHOLD_SETTING
+                WHERE MEMBER_ID = :memberId AND ITEM_CODE = :itemCode";
+
+            try
+            {
+                if (dbc.Connection.State != ConnectionState.Open) dbc.Connection.Open();
+
+                using (OracleCommand cmd = new OracleCommand(sql, dbc.Connection))
+                {
+                    cmd.Parameters.Add("memberId", OracleDbType.Varchar2).Value = mockMemberId;
+                    cmd.Parameters.Add("itemCode", OracleDbType.Varchar2).Value = itemCode;
+
+                    using (OracleDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // 2. 최대/최소값 가져와서 텍스트박스에 표시
+                            // DB의 NUMBER(10, 2)는 C#에서 decimal 또는 double로 읽을 수 있습니다.
+                            // 읽어온 값을 문자열로 변환하여 텍스트박스에 넣습니다.
+                            if (!reader.IsDBNull(0))
+                            {
+                                txtUpperBound.Text = reader.GetDecimal(0).ToString();
+                            }
+                            if (!reader.IsDBNull(1))
+                            {
+                                txtLowerBound.Text = reader.GetDecimal(1).ToString();
+                            }
+
+                            MessageBox.Show($"'{selectedItemName}'의 임계치가 성공적으로 조회되었습니다.", "조회 완료");
+                        }
+                        else
+                        {
+                            MessageBox.Show($"선택하신 항목({selectedItemName})에 대한 임계치 설정 정보가 없습니다.", "정보 없음");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"임계치 조회 중 오류 발생: {ex.Message}", "조회 오류");
+            }
+            finally
+            {
+                if (dbc.Connection.State == ConnectionState.Open) dbc.Connection.Close();
+            }
+        }
         // ===================================================================
         // HospitalAddbtn 클릭 이벤트 핸들러: 별도의 폼 없이 병원 기록 통합 조회 (요청 사항)
         // ===================================================================
         private void HospitalAddbtn_Click(object sender, EventArgs e)
         {
-            // HOSP_SYNC_DATA와 HOSPITAL을 JOIN하여 연동 병원의 이름만 조회합니다.
+            if (dbc == null)
+            {
+                MessageBox.Show("데이터베이스 연결 객체(dbc)가 초기화되지 않았습니다.", "FATAL 오류");
+                return;
+            }
+            if (dataGridHospitalLog == null)
+            {
+                MessageBox.Show("데이터그리드 컨트롤(dataGridHospitalLog)이 초기화되지 않았습니다.", "UI 오류");
+                return;
+            }
+
+            // 🚨 복원된 쿼리: HOSPITAL_INFO 테이블과의 JOIN을 포함
             string sql = @"
                 SELECT
                     H.HOSP_NAME AS ""병원명"",
-                    TO_CHAR(HSD.SYNC_DATE, 'YYYY-MM-DD HH24:MI:SS') AS ""연동일시""
+                    -- SYNC_DATE 컬럼의 FormatException을 방지하기 위해 명시적으로 TO_CHAR 사용
+                    TO_CHAR(HSD.SYNC_DATE, 'YYYY-MM-DD HH24:MI:SS') AS ""연동일시"",
+                    HSD.HOSP_CODE -- 다음 조회를 위해 코드도 함께 가져옵니다.
                 FROM HOSP_SYNC_DATA HSD
-                JOIN HOSPITAL H ON HSD.HOSP_CODE = H.HOSP_CODE
+                -- 🚨 스키마에 따라 HOSPITAL_INFO로 테이블 이름 변경
+                JOIN HOSPITAL_INFO H ON HSD.HOSP_CODE = H.HOSP_CODE
                 WHERE HSD.MEMBER_ID = :memberId
                 ORDER BY HSD.SYNC_DATE DESC";
 
             try
             {
-                // 데이터베이스 연결 및 조회
                 using (OracleDataAdapter adapter = new OracleDataAdapter(sql, dbc.Connection))
                 {
-                    // MEMBER_ID는 DB의 NUMBER 타입에 매핑하여 OracleDbType.Int32 사용
-                    adapter.SelectCommand.Parameters.Add("memberId", OracleDbType.Int32).Value = mockMemberId;
+                    // 🚨 수정: MEMBER_ID는 VARCHAR2 타입입니다.
+                    adapter.SelectCommand.Parameters.Add("memberId", OracleDbType.Varchar2).Value = mockMemberId;
 
                     DataTable dt = new DataTable();
 
-                    // Connection이 닫혀 있으면 엽니다.
                     if (dbc.Connection.State != ConnectionState.Open) dbc.Connection.Open();
-                    adapter.Fill(dt);
+                    adapter.Fill(dt); // 이 시점에서 FormatException이 발생하면 데이터 변환 문제일 가능성 높음
 
-                    // dataGridHospitalLog에 결과 바인딩
                     dataGridHospitalLog.DataSource = dt;
 
                     if (dt.Rows.Count > 0)
@@ -107,14 +207,18 @@ namespace PHR_Management_App
                     }
                 }
             }
+            catch (FormatException ex)
+            {
+                // FormatException이 다시 발생하면, DB의 다른 컬럼(예: SYNC_DATE가 아닌 다른 DATE 타입 컬럼)을 확인해야 합니다.
+                MessageBox.Show("데이터 변환 오류 (FormatException): 데이터베이스의 날짜/숫자 형식이 C# DataTable로 로드될 때 문제가 발생했습니다. \n\n" +
+                                $"상세 오류: {ex.Message}", "FormatException 진단");
+            }
             catch (Exception ex)
             {
-                // 오류 타입과 메시지를 정확히 출력하여 진단에 도움을 줍니다.
                 MessageBox.Show($"병원 연동 정보 조회 중 오류 발생 ({ex.GetType().Name}): {ex.Message}", "오류");
             }
             finally
             {
-                // 작업 완료 후 Connection을 닫습니다.
                 if (dbc.Connection.State == ConnectionState.Open) dbc.Connection.Close();
             }
         }
@@ -125,6 +229,11 @@ namespace PHR_Management_App
             DateTime measureDate = DateTime.Today;
 
             int count = 0;
+
+            // mockMemberId가 "M001"과 같은 문자열 ID라고 가정합니다.
+            // 만약 "1001"과 같은 숫자 ID를 문자열로 저장했다면, 아래 바인딩 타입만 Varchar2로 변경해도 작동합니다.
+            // 여기서는 VARCHAR2 타입으로 안전하게 바인딩하여 String.Format 오류를 방지합니다.
+            string memberIdString = mockMemberId.ToString();
 
             try
             {
@@ -148,8 +257,10 @@ namespace PHR_Management_App
                     }
 
                     // 3. 숫자로 변환 시도 (OracleException 방지)
+                    // 사용자 입력 (11, 111 등)이 정수든 실수든 decimal.TryParse는 잘 처리합니다.
                     if (!decimal.TryParse(rawValue, out decimal measureValue))
                     {
+                        // 이 블록은 'abc' 같은 문자가 들어왔을 때만 실행됩니다.
                         MessageBox.Show($"'{rawValue}'는 유효한 숫자가 아닙니다. '{txt.Name}'의 입력값을 확인해주세요. 이 경우 DB 접근을 시도하지 않습니다.", "입력 오류", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                         return;
                     }
@@ -160,17 +271,22 @@ namespace PHR_Management_App
                         cmd.Connection = dbc.Connection;
 
                         // MEASURE_LOG 테이블에 데이터 삽입 쿼리
-                        // SEQ_MEASURE_LOG 시퀀스 사용 가정
                         string insertQuery = @"
-                            INSERT INTO MEASURE_LOG 
-                                (LOG_ID, MEMBER_ID, ITEM_CODE, MEASURE_DATE, MEASURE_VALUE, INPUT_TYPE)
-                            VALUES 
-                                (SEQ_MEASURE_LOG.NEXTVAL, :memberId, :itemCode, :measureDate, :measureValue, 'U')
-                        ";
+                    INSERT INTO MEASURE_LOG 
+                        (LOG_ID, MEMBER_ID, ITEM_CODE, MEASURE_DATE, MEASURE_VALUE, INPUT_TYPE)
+                    VALUES 
+                        (SEQ_MEASURE_LOG.NEXTVAL, :memberId, :itemCode, :measureDate, :measureValue, 'U')
+                ";
                         cmd.CommandText = insertQuery;
 
-                        // 5. 파라미터 바인딩 (데이터 타입 오류 방지 및 MEMBER_ID(1001) 사용)
-                        cmd.Parameters.Add("memberId", OracleDbType.Int32).Value = mockMemberId;
+                        // 5. 파라미터 바인딩 수정 (MEMBER_ID의 타입 불일치 오류 방지)
+
+                        // 만약 MEMBER_ID 컬럼이 DB에서 VARCHAR2(20) 타입이라면:
+                        cmd.Parameters.Add("memberId", OracleDbType.Varchar2).Value = 1001;
+
+                        // 만약 MEMBER_ID 컬럼이 DB에서 NUMBER 타입이라면: (이 경우 mockMemberId가 반드시 숫자형 문자열이어야 함)
+                        // cmd.Parameters.Add("memberId", OracleDbType.Int32).Value = int.Parse(memberIdString); 
+
                         cmd.Parameters.Add("itemCode", OracleDbType.Varchar2).Value = itemCode;
                         cmd.Parameters.Add("measureDate", OracleDbType.Date).Value = measureDate;
                         // 측정 값은 소수점도 허용하는 OracleDbType.Decimal로 정확하게 바인딩
@@ -188,7 +304,7 @@ namespace PHR_Management_App
                 if (count > 0)
                 {
                     MessageBox.Show($"{count}개의 건강 데이터가 성공적으로 추가되었습니다.", "데이터 추가 성공");
-                    // LoadHealthLog(); 
+                    // LoadHealthLog();  // 측정 기록을 다시 로드하는 메서드를 여기에 호출할 수 있습니다.
                 }
                 else
                 {
@@ -200,7 +316,7 @@ namespace PHR_Management_App
             {
                 string errorMessage = $"Oracle DB 오류 ({oex.Number}): {oex.Message}";
 
-                // 주요 Oracle DB 오류 코드 진단
+                // 주요 Oracle DB 오류 코드 진단 (기존 진단 유지)
                 switch (oex.Number)
                 {
                     case 942:
@@ -210,13 +326,13 @@ namespace PHR_Management_App
                         errorMessage += "\n\n[진단: ORA-02289] 🚨 시퀀스 미존재\nSEQ_MEASURE_LOG 시퀀스가 DB에 정의되지 않았습니다. (SELECT SEQ_MEASURE_LOG.NEXTVAL FROM DUAL; 확인)";
                         break;
                     case 1400:
-                        errorMessage += "\n\n[진단: ORA-01400] 🚨 NULL 값 삽입\nNULL을 허용하지 않는 컬럼에 값이 빠졌거나 MEMBER_ID(1001)가 MEMBER_INFO에 없습니다. (FK 제약조건)";
+                        errorMessage += "\n\n[진단: ORA-01400] 🚨 NULL 값 삽입\nNULL을 허용하지 않는 컬럼에 값이 빠졌거나 MEMBER_ID가 MEMBER_INFO에 없습니다. (FK 제약조건)";
                         break;
                     case 1031:
-                        errorMessage += "\n\n[진단: ORA-01031] 🚨 권한 부족\n현재 DB 사용자(hong1)에게 INSERT 권한이 없습니다.";
+                        errorMessage += "\n\n[진단: ORA-01031] 🚨 권한 부족\n현재 DB 사용자에게 INSERT 권한이 없습니다.";
                         break;
                     case 1722:
-                        errorMessage += "\n\n[진단: ORA-01722] 🚨 숫자 오류\n컬럼에 들어갈 데이터 타입이 잘못되었습니다. MEMBER_ID(1001)가 NUMBER 타입인지 확인해주세요.";
+                        errorMessage += "\n\n[진단: ORA-01722] 🚨 숫자 오류\n컬럼에 들어갈 데이터 타입이 잘못되었습니다. MEMBER_ID 또는 MEASURE_VALUE의 DB 타입을 확인해주세요.";
                         break;
                     default:
                         errorMessage += "\n\n[진단]\nSQL 구문이나 DB 연결 정보를 다시 점검해주세요.";
@@ -227,8 +343,8 @@ namespace PHR_Management_App
             }
             catch (Exception ex)
             {
-                // 일반 오류 처리 (DB 연결 자체 문제 등)
-                MessageBox.Show($"일반 오류 발생: {ex.Message}", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // 🚨 일반 오류 처리 (여기서 "입력 문자열의 형식이 잘못되었습니다."가 잡힐 가능성이 높음)
+                MessageBox.Show($"일반 오류 발생: {ex.Message}\n\n[추가 진단: 입력 문자열 형식 오류]\n1. 'mockMemberId' 변수가 유효한 문자열인지 확인하세요. (예: M001 또는 1001)\n2. 해당 변수가 DB의 MEMBER_ID 컬럼 타입(VARCHAR2 또는 NUMBER)과 맞게 바인딩되었는지 확인하세요.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             finally
             {
@@ -348,12 +464,11 @@ namespace PHR_Management_App
             }
         }
 
-        private void Thresholdbtn_Click(object sender, EventArgs e)
-        {
+        
 
-        }
+       
 
-      
+
 
         // 이 메서드는 Designer.cs 파일에 정의되어야 하지만, 
         // 전체 코드 제공을 위해 최소한의 형태로 포함합니다. 
